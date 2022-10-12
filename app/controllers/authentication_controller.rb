c442cfae1188d5519123a4b0bc1084cee4de2920
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-10-12 23:31:02 +0300

    params

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,5 +5,6 @@ class AuthenticationController < ApplicationController
     end
     private 
     def create_params 
+        params.permit(:name, :email, :passowrd, :display_picture, :type)
     end
 end
