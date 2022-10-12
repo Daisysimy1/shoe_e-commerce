ac0c4aef831aef2a1d5bf61282d406427626b889
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-10-12 22:58:32 +0300

    validating email in user account

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,9 @@
+class AuthenticationController < ApplicationController
+    def create_account 
+    end
+    def login_account 
+    end
+    private 
+    def create_params 
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :shoes, dependant: :destroy
     has_many :carts, dependant: :destroy
     has_many :orders, dependant: :destroy
+    validates :email, uniqueness: true
 end
